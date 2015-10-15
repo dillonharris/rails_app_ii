bcb8c997c6a4050c2420ecad7f821c876f1087c6
Dillon Harris <harrisdillon0@gmail.com>
2015-10-15 15:29:53 +0200

    Finishes Custom Routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   
   resources :users
 
+  get "movies/filter/:scope" => "movies#index", as: :filtered_movies
+  
   root "movies#index"
   resources :movies do
     resources :reviews
